e99b839d430ca9fe58357f3bc5bffd00822dc4c8
Riccardo Mariani <Riccardo.Mariani@helpsystems.com>
2020-04-16 12:10:46 +0200

    docs: update

--- projects/odata-v4-ng-app/src/app/app.module.ts
+++ projects/odata-v4-ng-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { InputTextModule, TabViewModule } from 'primeng/primeng';
+import { InputTextModule, TabViewModule } from 'primeng';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
